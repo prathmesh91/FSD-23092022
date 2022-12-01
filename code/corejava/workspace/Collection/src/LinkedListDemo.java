9310d2b85aa892fc175fa6b2de02a1c68f725841
scn-tech <scnaikwadi2@gmail.com>
2022-12-01 08:03:08 +0530

    Set Interface Intro
    Set Interface Intro

--- code/corejava/workspace/Collection/src/LinkedListDemo.java
+++ code/corejava/workspace/Collection/src/LinkedListDemo.java
@@ -6,6 +6,32 @@ public class LinkedListDemo {
 		list.add(123);
 		list.add("Abc");
 		list.add(1.23);
+		System.out.println("Original List : "+ list);
+		
+		System.out.println(list.get(1));
+		
+		list.push("Xyz"); // Add element at first position
+		System.out.println("After Push : "+list);
+		
+		System.out.println(list.pop());// select and remove the first element
+		System.out.println("After Pop : "+list);
+		
+		// select the first element but not removes it, if 1st element is not present
+		// them it will return null
+		System.out.println(list.peek()); 
+		System.out.println("After Peek : "+list);
+		
+		// select and removes the first element. it will return null if no first element present.
+		System.out.println(list.poll());
+		System.out.println("After Poll : "+list);
+		
+		System.out.println(list.getFirst());
+		System.out.println(list.getLast());
+		
+		System.out.println(list.removeFirst());
+		System.out.println(list.removeLast());
 		System.out.println(list);
+		
+		
 	}
 }
