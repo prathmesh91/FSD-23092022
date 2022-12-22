a87053fea4fc2fa85e7dcf65eef074aeb3872047
scn-tech <scnaikwadi2@gmail.com>
2022-12-22 10:19:27 +0530

    Servlet Intro
    Servlet Intro

---
+++ code/advjava/first-web-app/src/main/java/ServletIntro.java
@@ -0,0 +1,17 @@
+
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/servlet-intro")
+public class ServletIntro extends HttpServlet {
+
+	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		System.out.println("Welcome To First Servlet......");
+	}
+
+}
